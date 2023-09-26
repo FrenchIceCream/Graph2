dcaefcce4df5a071ad08f9ffb4f8c7ba72e4fe8c
andre <andrew260602sush@gmail.com>
2023-09-26 21:13:42 +0300

    вскрытие

--- Form1.cs
+++ Form1.cs
@@ -33,11 +33,25 @@ namespace Graph2
             }
         }
 
-        private void DrawPoint(int x, int y, float a)
+        private void DrawPoint(bool steep, int x, int y, float a)
         {
             Pen pen = new Pen(Color.FromArgb((int)(a * 255), 255, 0, 0), 1);
-            g.DrawRectangle(pen, new Rectangle(x, y, 1, 1));
+            if (steep)
+            {
+                g.DrawRectangle(pen, new Rectangle(y, x, 1, 1));
 
+            }
+            else
+            {
+                g.DrawRectangle(pen, new Rectangle(x, y, 1, 1));
+
+            }
+
+        }
+
+        private void Swap(ref int x1,ref int x2)
+        {
+            int x = x1; x1 = x2; x2 = x;
         }
 
         /*private void line(int x0, int x1, int y0, int y1)
@@ -53,29 +67,7 @@ namespace Graph2
                 y += gradient;
             }
         }*/
-        private void line(int x0, int x1, int y0, int y1)
-        {
-
-            int sx = x0 > x1 ? -1 : 1;
-            int sy = y0 > y1 ? -1 : 1;
-
-            float dy = Math.Abs(y1 - y0);
-            float dx = Math.Abs(x1 - x0);
-            bool gradient2 = Math.Abs(dy / (double)dx) <= 1;
-            float gradient = dy / dx;
-
-
-
-            float y = y0 + gradient;
-            for (var x = x0 + 1; x <= x1 - 1; x++)
-            {
-                DrawPoint(x, (int)y, 1 - (y - (int)y));
-                DrawPoint(x, (int)y + 1, y - (int)y);
-                y += gradient;
-            }
-
-
-        }
+    
 
         /*private void line(int x0, int x1, int y0, int y1)
         {
@@ -101,75 +93,31 @@ namespace Graph2
             }
         }*/
 
-        void DrawWuLine(int x0, int y0, int x1, int y1)
+        private void WuLine(int x0, int y0, int x1, int y1)
         {
-            //Вычисление изменения координат
-            int dx = (x1 > x0) ? (x1 - x0) : (x0 - x1);
-            int dy = (y1 > y0) ? (y1 - y0) : (y0 - y1);
-            //Если линия параллельна одной из осей, рисуем обычную линию - заполняем все пикселы в ряд
-            if (dx == 0 || dy == 0)
+            var steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
             {
-                g.DrawLine(new Pen(Color.Red), x0, y0, x1, y1);
-                return;
+                Swap(ref x0, ref y0);
+                Swap(ref x1, ref y1);
+            }
+            if (x0 > x1)
+            {
+                Swap(ref x0, ref x1);
+                Swap(ref y0, ref y1);
             }
 
-            //Для Х-линии (коэффициент наклона < 1)
-            if (dy < dx)
+            DrawPoint(steep, x0, y0, 1); // Эта функция автоматом меняет координаты местами в зависимости от переменной steep
+            DrawPoint(steep, x1, y1, 1); // Последний аргумент — интенсивность в долях единицы
+            float dx = x1 - x0;
+            float dy = y1 - y0;
+            float gradient = dy / dx;
+            float y = y0 + gradient;
+            for (var x = x0 + 1; x <= x1 - 1; x++)
             {
-                //Первая точка должна иметь меньшую координату Х
-                if (x1 < x0)
-                {
-                    x1 += x0; x0 = x1 - x0; x1 -= x0;
-                    y1 += y0; y0 = y1 - y0; y1 -= y0;
-                }
-                //Относительное изменение координаты Y
-                float grad = (float)dy / dx;
-                //Промежуточная переменная для Y
-                float intery = y0 + grad;
-                //Первая точка
-                DrawPoint(x0, y0, 1f);
-
-                for (int x = x0 + 1; x < x1; x++)
-                {
-                    //Верхняя точка
-                    DrawPoint(x, (int)(intery), (1 - (intery % 1)));
-                    //Нижняя точка
-                    DrawPoint(x, (int)(intery) + 1, (intery %1));
-                    //Изменение координаты Y
-                    intery += grad;
-                }
-
-                //Последняя точка
-                DrawPoint(x1, y1, 1);
-            }
-
-            //Для Y-линии (коэффициент наклона > 1)
-            else
-            {
-                //Первая точка должна иметь меньшую координату Y
-                if (y1 < y0)
-                {
-                    x1 += x0; x0 = x1 - x0; x1 -= x0;
-                    y1 += y0; y0 = y1 - y0; y1 -= y0;
-                }
-                //Относительное изменение координаты X
-                float grad = (float)dx / dy;
-                //Промежуточная переменная для X
-                float interx = x0 + grad;
-                //Первая точка
-                DrawPoint(x0, y0, 1);
-
-                for (int y = y0 + 1; y < y1; y++)
-                {
-                    //Верхняя точка
-                    DrawPoint((int)(interx), y, 1 - (interx %1));
-                    //Нижняя точка
-                    DrawPoint((int)(interx) + 1, y, (interx % 1));
-                    //Изменение координаты X
-                    interx += grad;
-                }
-                //Последняя точка
-                DrawPoint(x1, y1, 1);
+                DrawPoint(steep, x, (int)y, 1 - (y - (int)y));
+                DrawPoint(steep, x, (int)y + 1, y - (int)y);
+                y += gradient;
             }
         }
 
@@ -183,7 +131,7 @@ namespace Graph2
             Pen pen_r = new Pen(Color.Red, 1);
             //g.DrawRectangle(pen_r, new Rectangle(10, 10, 1, 1));
 
-            DrawWuLine(firstPoint.Value.X, newPoint.X, firstPoint.Value.Y, newPoint.Y);
+            WuLine(firstPoint.Value.X, newPoint.X, firstPoint.Value.Y, newPoint.Y);
             /*
             int x1 = firstPoint.Value.X; int y1 = firstPoint.Value.Y;
             int x2 = newPoint.X; int y2 = newPoint.Y;
